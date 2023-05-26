6515628fdb1810cb9fe1ea2e88b177f17bf74a77
amirwpf <hh1900dppp@gmail.com>
2023-05-26 15:16:49 +0430

    User class added

---
+++ HW4/Entities/User.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.Entities
+{
+    public class User
+    {
+        [Name("Phone")]
+        private string phone;
+        [Name("Birth Date")]
+        private DateTime birthDate;
+
+        [Name("Name")]
+        public string Name { get; set; }
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                if(value.Length==11)
+                    phone = value;
+            }
+        }
+        public DateTime Birthdate
+        {
+            get { return birthDate; }
+            set
+            {
+                if (value<DateTime.Now)
+                    birthDate = value;
+            }
+        }
+    }
+}
